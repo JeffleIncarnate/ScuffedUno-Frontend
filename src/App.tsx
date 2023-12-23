e04e7a42de92c39dc2d279f595c0a8d31b05bcb1
Dhruv Rayat <dhruvrayat50@gmail.com>
2023-12-23 20:30:45 +1300

    style: moved navbar out of each page component and to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import "./index.scss";
 
 import { Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
 // Routes
 import HomePage from "./pages/home";
 import SetupPage from "./pages/setup";
+import Navbar from "./components/navbar/navbar";
 
 function App() {
   return (
     <>
+      <Navbar />
+      <ToastContainer position="top-right" theme="colored" />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/setup" element={<SetupPage />} />
