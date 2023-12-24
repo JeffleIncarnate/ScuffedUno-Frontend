312a6b97d3be3a044f954b9df2565cf89df649b8
Dhruv Rayat <dhruvrayat50@gmail.com>
2023-12-24 18:06:27 +1300

    feat: strict mode be like: :moyai:

--- src/main.tsx
+++ src/main.tsx
@@ -8,11 +8,9 @@ import { Provider } from "react-redux";
 import { store } from "./state/store.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
 );
